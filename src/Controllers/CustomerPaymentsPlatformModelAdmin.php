10db51a0684f67075906689a667d136fba1c0c01
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-05-31 10:09:15 +1000

    Clean up modeladmin, remove GridField actions that are unnecessary. Define no model importers, set import form to false

--- src/Controllers/CustomerPaymentsPlatformModelAdmin.php
+++ src/Controllers/CustomerPaymentsPlatformModelAdmin.php
@@ -3,7 +3,18 @@
 namespace NSWDPC\Payments\NSWGOVCPP\Agency;
 
 use Silverstripe\Admin\ModelAdmin;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
+use SilverStripe\Forms\GridField\GridFieldDataColumns;
+use SilverStripe\Forms\GridField\GridFieldImportButton;
+use SilverStripe\Forms\GridField\GridFieldPrintButton;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
+use SilverStripe\Forms\GridField\GridFieldExportButton;
+use SilverStripe\Forms\GridField\GridFieldEditButton;
+use SilverStripe\Forms\GridField\GridFieldDetailForm;
+use SilverStripe\Forms\GridField\GridFieldAddExistingAutoCompleter;
 use SilverStripe\Omnipay\Model\Payment as OmnipayPayment;
+
 
 /**
  * Provide an administration are to view payments
@@ -11,10 +22,25 @@ use SilverStripe\Omnipay\Model\Payment as OmnipayPayment;
  */
 class CustomerPaymentsPlatformModelAdmin extends ModelAdmin
 {
+
+    /**
+     * @inheritdoc
+     */
+    public $showImportForm = false;
+
+    /**
+     * @var string
+     */
     private static $url_segment = 'cpp';
 
+    /**
+     * @var string
+     */
     private static $menu_icon_class = 'font-icon-tags';
 
+    /**
+     * @var array
+     */
     private static $managed_models = [
         Payment::class,
         OmnipayPayment::class,
@@ -22,4 +48,33 @@ class CustomerPaymentsPlatformModelAdmin extends ModelAdmin
     ];
 
     private static $menu_title = 'CPP';
+
+    /**
+     * @inheritdoc
+     */
+    public function getModelImporters()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getEditForm($id = null, $fields = null)
+    {
+        $form = parent::getEditForm($id, $fields);
+        if($grid = $form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass))) {
+            $config = $grid->getConfig();
+            if($config) {
+                $config->removeComponentsByType(GridFieldAddNewButton::class);
+                $config->removeComponentsByType(GridFieldPrintButton::class);
+                $config->removeComponentsByType(GridFieldImportButton::class);
+                $config->removeComponentsByType(GridFieldAddExistingAutoCompleter::class);
+                if($this->modelClass == Configuration::class) {
+                    $config->removeComponentsByType(GridFieldExportButton::class);
+                }
+            }
+        }
+        return $form;
+    }
 }
